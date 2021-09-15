975d24aaad9a3284e930df83a2511469aa48d463
masamichi-iba <89885324+masamichi-iba@users.noreply.github.com>
2021-09-15 14:57:56 +0900

    Update main2.js

--- assets/js/main2.js
+++ assets/js/main2.js
@@ -25,6 +25,12 @@ const app = Vue.createApp({
           // q2: 'えええ',
           // q3: 'おおお'
         },
+        stage3: {
+          q1: 'ううう',
+          // q2: 'かかか',
+          // q3: 'ききき',
+        }
+      },
 
       /* それぞれの問題が正解かどうか
       *  ex. 問題2-3を追加する場合は配列にfalseを追加します。
@@ -38,11 +44,18 @@ const app = Vue.createApp({
           // false, // 2-2
           // false, // 2-3
         ],
+        stage3: [
+          false, // 3-1
+          // false, // 3-2
+          // false, // 3-3
+        ]
+      },
 
       /* ステージの問題が全て正解かどうか */
       clear: {
         stage1: false,
         stage2: false,
+        stage3: false,
       },
 
       /* 次のステージを表示するかどうか
